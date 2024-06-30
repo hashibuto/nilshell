a8855e3f31f202d231adfc022d3efc828bc3460d
= <=>
2024-06-30 16:20:00 +0900

    added term package

--- utils_test.go
+++ utils_test.go
@@ -1,29 +1,10 @@
 package ns
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestPaddingRight(t *testing.T) {
-	x := PadRight("hello", 20, 2)
-	assert.True(t, strings.HasPrefix(x, "hello "))
-	assert.True(t, len(x) == 20)
-}
-
-func TestPaddingRightGutters(t *testing.T) {
-	x := PadRight("hello", 7, 2)
-	assert.True(t, strings.HasPrefix(x, "hello "))
-	assert.True(t, len(x) == 7)
-}
-
-func TestPaddingRightGutters2(t *testing.T) {
-	x := PadRight("hello", 6, 2)
-	assert.True(t, strings.HasPrefix(x, "h... "))
-	assert.True(t, len(x) == 6)
-}
 
 func TestCalculateColumnWidth(t *testing.T) {
 	width, numCols := CalculateColumnWidth([]string{"hello", "world", "dog", "cat"}, 80, 2, 2)
